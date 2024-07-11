a6f896127d387ac84507c7e791236ec5484df7fd
glowcoder1 <tefeglory.official@gmail.com>
2024-07-11 11:39:43 +0100

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+an asynchronous coroutine that takes in an integer argument 
+(max_delay, with a default value of 10)
+waits for a random delay between 0 and max_delay
+ seconds and eventually returns it.
+"""
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+  """
+  waits for a random delay between 0 and max_delay
+  seconds and eventually returns it.
+  """
+  delay = random.uniform(0, max_delay)
+  await asyncio.sleep(delay)
+  return delay
