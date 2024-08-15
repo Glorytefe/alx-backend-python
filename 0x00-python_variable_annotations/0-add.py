ee1b6f8df29ac7656eed7d698e372aaa94ade892
glowcoder1 <tefeglory.official@gmail.com>
2024-08-15 23:44:00 +0100

    doc strings for task 0 and task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+"""add module"""
+
 
 def add(a: float, b: float) -> float:
+    """adds to float"""
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+concat module
+"""
+
 
 def concat(str1: str, str2: str) -> str:
+    """concatenates two strings"""
     return str1 + str2
