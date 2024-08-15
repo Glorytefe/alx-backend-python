5f820e08558cad6027fc3d66e4a701ffaef3bacb
glowcoder1 <tefeglory.official@gmail.com>
2024-08-15 23:59:36 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from typing import List
+
+"""complec types - list of floats"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns sum of input_list"""
+    return sum(input_list)
